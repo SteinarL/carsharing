e56e9a0199f44b63b3cc4900b240b9f11245883a
SteinarL <66571725+SteinarL@users.noreply.github.com>
2022-12-05 19:26:27 +0000

    Update Carsharing_5.12.py

--- Carsharing_5.12.py
+++ Carsharing_5.12.py
@@ -3,38 +3,41 @@ import streamlit as st
 original_title = '<p style="font-family:Helvetica; color:#458B74; font-size: 30px;">Welcome to the NOVA rideshare platform!</p>'
 st.markdown(original_title, unsafe_allow_html=True)
 
+tabs = st.tabs(["Passengers", "Drivers"])
 
-driver_dictionary = {
-        "Departure": "Santos",
-        "Destination": "Nova SBE",
-        "Departure_Time": "4 PM",
-        "Free_capacity": 4,
-        "Passengers": [],
-        "Full": False
-  }
+tab_passengers = tabs[0]
+with tab_passengers:
+        driver_dictionary = {
+                "Departure": "Santos",
+                "Destination": "Nova SBE",
+                "Departure_Time": "4 PM",
+                "Free_capacity": 4,
+                "Passengers": [],
+                "Full": False
+          }
 
-# SELECTION OF DEPARTURE TIME
-genre = st.radio(
-    "When would you need a ride?",
-    ("8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "4 PM", "8 PM"))
- 
-if genre in driver_dictionary["Departure_Time"]:
-    st.write("Slot secured.") 
-          
-else: 
-    st.write("No drivers available.")
+        # SELECTION OF DEPARTURE TIME
+        genre = st.radio(
+            "When would you need a ride?",
+            ("8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "4 PM", "8 PM"))
 
-# SELECTION OF DESTINATION
-genre = st.radio(
-        "What is your destination?",
-        ("Nova SBE", "Santos"))
-if genre in driver_dictionary["Destination"]:
-        st.write("Driver available for selected destination.")
+        if genre in driver_dictionary["Departure_Time"]:
+            st.write("Slot secured.") 
 
-else:
-    st.write("No driver available for selected destination.")
-        
-        
+        else: 
+            st.write("No drivers available.")
+
+        # SELECTION OF DESTINATION
+        genre = st.radio(
+                "What is your destination?",
+                ("Nova SBE", "Santos"))
+        if genre in driver_dictionary["Destination"]:
+                st.write("Driver available for selected destination.")
+
+        else:
+            st.write("No driver available for selected destination.")
+tab_drivers = tabs[1]
+with tab_drivers:        
         
 
 
