c4b29e4c9cfa9b8d37bc62b46ca1ac50e35da1b7
nojoze <119888400+nojoze@users.noreply.github.com>
2022-12-05 18:58:49 +0000

    Update Carsharing_5.12.py

--- Carsharing_5.12.py
+++ Carsharing_5.12.py
@@ -15,7 +15,7 @@ driver_dictionary = {
 
 genre = st.radio(
     "When would you need a ride?",
-    ("8 AM", "12 PM", "4 PM", "8 PM"))
+    ("8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "4 PM", "8 PM"))
  
 if genre in driver_dictionary["Departure_Time"]:
     st.write("Slot secured.") 
