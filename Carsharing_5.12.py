91b79d4f013c9e01f54079653295d92673d6c69f
SteinarL <66571725+SteinarL@users.noreply.github.com>
2022-12-05 22:24:59 +0000

    Update Carsharing_5.12.py

--- Carsharing_5.12.py
+++ Carsharing_5.12.py
@@ -3,44 +3,50 @@ import streamlit as st
 original_title = '<p style="font-family:Helvetica; color:#458B74; font-size: 30px;">Welcome to the NOVA rideshare platform!</p>'
 st.markdown(original_title, unsafe_allow_html=True)
 
+driver_dictionary = {
+        "Departure": "Santos",
+        "Destination": "Nova SBE",
+        "Departure_Time": "4 PM",
+        "Free_capacity": 4,
+        "Passengers": [],
+        "Full": False
+        }
+
+
 tabs = st.tabs(["Passengers", "Drivers"])
 
 tab_passengers = tabs[0]
 with tab_passengers:
-# I indented everything so have it inside a tab. Then we can have one tab for passengeres and one for drivers
-        driver_dictionary = {
-                "Departure": "Santos",
-                "Destination": "Nova SBE",
-                "Departure_Time": "4 PM",
-                "Free_capacity": 4,
-                "Passengers": [],
-                "Full": False
-          }
+        
+        with st.form(key='my_form'):
+                
+                st.write("Please select the wished time of departure and your destination.")
+                # SELECTION OF DEPARTURE TIME
+                passenger_dept_time = st.radio(
+                    "When would you need a ride?",
+                    ("8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "4 PM", "8 PM"))
 
-        # SELECTION OF DEPARTURE TIME
-        passenger_dept_time = st.radio(
-            "When would you need a ride?",
-            ("8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "4 PM", "8 PM"))
+                if passenger_dept_time in driver_dictionary["Departure_Time"]:
+                    st.write("Slot secured.") 
+                else: 
+                    st.write("No drivers available.")
 
-        if passenger_dept_time in driver_dictionary["Departure_Time"]:
-            st.write("Slot secured.") 
+                # SELECTION OF DESTINATION
+                passenger_destination = st.radio(
+                        "What is your destination?",
+                        ("Nova SBE", "Santos"))
+                if passenger_destination in driver_dictionary["Destination"]:
+                        st.write("Driver available for selected destination.")
+                else:
+                    st.write("No driver available for selected destination.")
 
-        else: 
-            st.write("No drivers available.")
-
-        # SELECTION OF DESTINATION
-        passenger_destination = st.radio(
-                "What is your destination?",
-                ("Nova SBE", "Santos"))
-        if passenger_destination in driver_dictionary["Destination"]:
-                st.write("Driver available for selected destination.")
-
-        else:
-            st.write("No driver available for selected destination.")
-
+                submit_button = st.form_submit_button(label='Submit your need')
+                if submit_button:
+                        st.write("Your need has been submitted.")
+                
 tab_drivers = tabs[1]
 with tab_drivers:
-        # Selection of driver availability
+        # SELECTION OF DRIVER AVAILABILITY
         # Just a copy form above. Hasn't been adapted to drivers yet. 
                 # Trying to figure out how to append new times to dictionary when driver selects departure time. 
         driver_time = st.radio(
