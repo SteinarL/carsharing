20882e39df37e4a76c2624c4678a3db4e83361c6
SteinarL <66571725+SteinarL@users.noreply.github.com>
2022-12-08 18:17:59 +0000

    Update Carsharing_5.12.py

--- Carsharing_5.12.py
+++ Carsharing_5.12.py
@@ -15,7 +15,7 @@ with tab_drivers:
         driver_name = st.text_input("Please enter your name:")
         driver_departure = st.selectbox("From where do you want to leave?", ("Santos", "Nova SBE"))
         driver_arrival = st.selectbox("Where do you want to go?", ("Nova SBE", "Santos"))
-        driver_time = st.selectbox("At what time will you leave?", ("8 AM", "12 PM", "4 PM", "8 PM"))
+        driver_time = st.selectbox("At what time will you leave?", ("12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM"))
         driver_cap = st.number_input("How many free spots do you have?", min_value=1, step=1)
 
         #5: Ask if all entered data was correct. If yes, we end the loop and inputs are added to drivers_input dictionary:
@@ -33,7 +33,7 @@ with tab_passengers:
                           #4: Add the [] to the dictionary as Key, and add the inputs as values:
         passenger_departure = st.selectbox("From where do you want to leave?", ("Santos", "Nova SBE"))
         passenger_arrival = st.selectbox("Where do you want to go?", ("Nova SBE", "Santos"))
-        passenger_time = st.selectbox("At what time will you leave?", ("8 AM", "12 PM", "4 PM", "8 PM"))
+        passenger_time = st.selectbox("At what time will you leave?", ("12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM"))
 
         #5: Ask if all entered data was correct. If yes, we end the loop and inputs are added to drivers_input dictionary:
                   
