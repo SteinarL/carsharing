8da2d53b40984f9605187c73c33edd9017f113a1
SteinarL <66571725+SteinarL@users.noreply.github.com>
2022-12-08 11:04:21 +0000

    Update Carsharing_5.12.py

--- Carsharing_5.12.py
+++ Carsharing_5.12.py
@@ -18,18 +18,18 @@ tabs = st.tabs(["Drivers", "Passengers"])
 
 tab_drivers = tabs[0]
 
-    with st.form(key="my_form"):
-#4: Add the [] to the dictionary as Key, and add the inputs as values:
-        drivers_input['Name'] = st.text_input("Please enter your name:", key=1)
-        drivers_input['Departure'] = st.selectbox("From where do you want to leave?", ("Santos", "Nova SBE"))
-        drivers_input['Arrival'] = st.selectbox("Where do you want to go?", ("Nova SBE", "Santos"))
-        drivers_input['Departure_time'] = st.selectbox("At what time will you leave?", ("8 AM", "12 PM", "4 PM", "8 PM"))
-        drivers_input['Free_capacity'] = st.number_input("How many free spots do you have?", min_value=1, max_value=10, step=1)
+        with st.form(key="my_form"):
+    #4: Add the [] to the dictionary as Key, and add the inputs as values:
+            drivers_input['Name'] = st.text_input("Please enter your name:", key=1)
+            drivers_input['Departure'] = st.selectbox("From where do you want to leave?", ("Santos", "Nova SBE"))
+            drivers_input['Arrival'] = st.selectbox("Where do you want to go?", ("Nova SBE", "Santos"))
+            drivers_input['Departure_time'] = st.selectbox("At what time will you leave?", ("8 AM", "12 PM", "4 PM", "8 PM"))
+            drivers_input['Free_capacity'] = st.number_input("How many free spots do you have?", min_value=1, max_value=10, step=1)
 
-#5: Ask if all entered data was correct. If yes, we end the loop and inputs are added to drivers_input dictionary:
-        correct_input = st.form_submit_button(label="Publish ride")
-        if correct_input: 
-            drivers_open = False
+    #5: Ask if all entered data was correct. If yes, we end the loop and inputs are added to drivers_input dictionary:
+            correct_input = st.form_submit_button(label="Publish ride")
+            if correct_input: 
+                drivers_open = False
     
 #5: Ask if all entered data was correct. If yes, we end the loop and inputs are added to drivers_input dictionary:
     #correct_input = st.button("Do you want to save and publish this ride?")
