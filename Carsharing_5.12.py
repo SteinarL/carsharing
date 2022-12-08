51cb07ed442bccfb37048c9984bf4bd8f29fb506
SteinarL <66571725+SteinarL@users.noreply.github.com>
2022-12-08 11:07:24 +0000

    Update Carsharing_5.12.py

--- Carsharing_5.12.py
+++ Carsharing_5.12.py
@@ -28,4 +28,5 @@ while drivers_open:
 
     #5: Ask if all entered data was correct. If yes, we end the loop and inputs are added to drivers_input dictionary:
             correct_input = st.form_submit_button(label="Publish ride")
-                if correct_unput:
+            if correct_input:
+                st.write("Ride is submitted")
