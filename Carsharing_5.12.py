953e37428dd8009a0ef3e4d9ad0c31a5af7f4937
SteinarL <66571725+SteinarL@users.noreply.github.com>
2022-12-08 11:13:47 +0000

    Update Carsharing_5.12.py

--- Carsharing_5.12.py
+++ Carsharing_5.12.py
@@ -18,7 +18,7 @@ while drivers_open:
 
         tab_drivers = tabs[0]
 
-        with st.form(key="my_form"):
+     
     #4: Add the [] to the dictionary as Key, and add the inputs as values:
             drivers_input['Name'] = st.text_input("Please enter your name:", key=1)
             drivers_input['Departure'] = st.selectbox("From where do you want to leave?", ("Santos", "Nova SBE"))
@@ -27,16 +27,12 @@ while drivers_open:
             drivers_input['Free_capacity'] = st.number_input("How many free spots do you have?", min_value=1, max_value=10, step=1)
 
     #5: Ask if all entered data was correct. If yes, we end the loop and inputs are added to drivers_input dictionary:
-            correct_input = st.form_submit_button(label="Publish ride")
-            if correct_input:
-                st.write("Ride is published.")
+       
         
         tab_passengers = tabs[1]
         
-        with st.form(key="my_form"):
+      
             requests_input['Departure_P'] = st.selectbox("From where do you want to leave?", ("Santos", "Nova SBE"))
             requests_input['Arrival_P'] = st.selectbox("From where do you want to leave?", ("Nova SBE", "Santos"))
             requests_input['Departure_time_P'] = st.selectbox("At what time do you wish to leave?", ("8 AM", "12 PM", "4 PM", "8 PM"))
-            correct_input = st.form_submit_button(label="Request ride")
-            if correct_input:
-                st.write("Ride is requested.")
+         
