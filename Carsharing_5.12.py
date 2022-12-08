767e98fa8af0583e86dfb7181421498581492885
SteinarL <66571725+SteinarL@users.noreply.github.com>
2022-12-08 11:24:49 +0000

    Update Carsharing_5.12.py

--- Carsharing_5.12.py
+++ Carsharing_5.12.py
@@ -2,17 +2,27 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+#1: Create a new bridging dictionary:
+drivers_input = {}
+drivers_list = []
 
-tab1, tab2 = st.tabs(["Drivers", "Passengers"])
+#2: Loop or inputs will continue to run as long as it changes to False:
+drivers_open = True
 
-with tab1:
-     
-    #4: Add the [] to the dictionary as Key, and add the inputs as values:
-    drivers_input['Name'] = st.text_input("Please enter your name:")
-    drivers_input['Departure'] = st.selectbox("From where do you want to leave?", ("Santos", "Nova SBE"))
-    drivers_input['Arrival'] = st.selectbox("Where do you want to go?", ("Nova SBE", "Santos"))
-    drivers_input['Departure_time'] = st.selectbox("At what time will you leave?", ("8 AM", "12 PM", "4 PM", "8 PM"))
-    drivers_input['Free_capacity'] = st.number_input("How many free spots do you have?", min_value=1, max_value=10, step=1)
+#3: While True it will:
+#4: Add the [] to the dictionary as Key, and add the inputs as values:
+while drivers_open:
+
+     tab1, tab2 = st.tabs(["Drivers", "Passengers"])
+
+     with tab1:
+
+         #4: Add the [] to the dictionary as Key, and add the inputs as values:
+         drivers_input['Name'] = st.text_input("Please enter your name:")
+         drivers_input['Departure'] = st.selectbox("From where do you want to leave?", ("Santos", "Nova SBE"))
+         drivers_input['Arrival'] = st.selectbox("Where do you want to go?", ("Nova SBE", "Santos"))
+         drivers_input['Departure_time'] = st.selectbox("At what time will you leave?", ("8 AM", "12 PM", "4 PM", "8 PM"))
+         drivers_input['Free_capacity'] = st.number_input("How many free spots do you have?", min_value=1, max_value=10, step=1)
 
     #5: Ask if all entered data was correct. If yes, we end the loop and inputs are added to drivers_input dictionary:
            
